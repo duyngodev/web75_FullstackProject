26f47d77137ffa3de77e5ea86808b8882f4b8e91
Ngô Bảo Duy <124792803+duyngodev@users.noreply.github.com>
2024-03-07 11:18:16 +0700

    Update ProductDetail.jsx

--- frontend/src/pages/ProductDetail.jsx
+++ frontend/src/pages/ProductDetail.jsx
@@ -1,10 +1,10 @@
-import SingleProduct from "../Components/nhandev/SingleProduct";
+import SingleProduct from "../Components/nhandev/SingleProduct.jsx";
 import React, { useContext, useEffect, useState } from "react";
 import { ApiStateContext } from '../Components/nhandev/ApiStateProvider';
 import { useParams, useRoutes } from "react-router-dom";
 import '../Components/nhandev/ProductDetail.scss'
 import '../Components/nhandev/Swiper.scss'
-import ListSingleProduct from "../Components/nhandev/ListSingleProduct";
+import ListSingleProduct from "../Components/nhandev/ListSingleProduct.jsx";
 const ProductDetail = () => {
 
   const [data, setData] = useState([]);
