5947437fcc0cbf3fc66f82f419d03b997107ade6
Ngô Bảo Duy <124792803+duyngodev@users.noreply.github.com>
2024-03-07 11:18:43 +0700

    Update ProductList.jsx

--- frontend/src/pages/ProductList.jsx
+++ frontend/src/pages/ProductList.jsx
@@ -1,6 +1,6 @@
 import { Box, Button, Checkbox, Container, Divider, FormControl, FormControlLabel, FormGroup, FormLabel, Grid, IconButton, Radio, RadioGroup, Stack, Typography } from '@mui/material'
-import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
-import BookmarkIcon from '@mui/icons-material/Bookmark';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder.jsx';
+import BookmarkIcon from '@mui/icons-material/Bookmark.jsx';
 import { useState } from 'react'
 import { useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
